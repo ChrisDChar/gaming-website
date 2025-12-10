8cfabf14886ebbf5a74a21feeed0f62f2c5047f9
anora-hub <dildorauskanova171@gmail.com>
2025-12-10 16:51:39 +0500

    initial news

--- src/components/newsletter.jsx
+++ src/components/newsletter.jsx
@@ -1,43 +1,229 @@
+
+import HeroCard from "../pages/news-page/hero";
+import rasm from "../../assets/HomeIcons/rasm1.png";
+
+
 export default function Newsletter() {
-  return (
-    <section className="w-full px-4 py-12 bg-[#0f0b2c] text-white">
-      <div className="max-w-6xl mx-auto flex flex-col gap-10">
+    return (
 
-        <div className="flex flex-col justify-center">
-          <h1 className="font-poppins font-semibold text-[30px] md:text-[36px] leading-[1.8]">
-            Lorem Ipsum
-          </h1>
-          <p className="font-poppins font-normal text-[16px] md:text-[18px] leading-[1.8] text-gray-300 mt-4">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          </p>
-        </div>
+        <section className="w-full px-4 py-12 bg-[#0f0b2c] text-white">
+            <div className="max-w-6xl mx-auto ">
 
-        <div className="bg-white rounded-xl p-6 md:p-10 shadow-lg flex flex-col md:flex-row md:items-center md:justify-between gap-4 md:gap-6">
-          <div className="flex-1">
-            <h2 className="font-poppins font-medium text-[24px] md:text-[30px] leading-[1.2] text-black">
-              Stay in the loop
-            </h2>
-            <p className="font-poppins font-normal text-[16px] md:text-[17px] leading-[1.5] text-black mt-2 max-w-md">
-              Subscribe to receive the latest news and updates about TDA.  
-              We promise not to spam you!
-            </p>
-          </div>
+                <div className="text-center">
+                    <p className="pt-[50px] text-[20px] ">Home  <span className="text-[red]/40">News</span></p>
+                    <h1 className="text-[34px] font-bold pt-[15px]">Lorem Ipsum is simply dummy text of the printing and.</h1>
+                    <p className="pt-[15px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                </div>
+                <div class="w-full flex justify-center mt-20">
+                    <div class="w-full max-w-md">
+                        <div class="relative">
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                class="w-full py-3 pl-12 pr-4 rounded-xl bg-white border border-white/20 
+               text-black placeholder-black focus:outline-none 
+              "
+                            />
+                            <svg class="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-black"
+                                fill="none" stroke="currentColor" stroke-width="2"
+                                viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M21 21l-4.35-4.35M10 18a8 8 0 100-16 8 8 0 000 16z" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
 
-          <form className="flex w-full md:w-auto flex-col md:flex-row gap-3 md:gap-0 mt-4 md:mt-0">
-            <input
-              type="email"
-              placeholder="Enter email address"
-              className="flex-1 h-16 md:h-14 bg-black text-white placeholder-gray-400 rounded-l-md px-4 outline-none font-poppins w-full text-lg md:text-base"
-            />
-            <button
-              type="submit"
-              className="h-8 md:h-14 px-4 bg-red-600 text-white font-poppins font-medium  rounded-r-md hover:bg-red-700 transition w-full md:w-auto text-base md:text-base"
-            >
-              Continue
-            </button>
-          </form>
-        </div>
-      </div>
-    </section>
-  );
+
+
+
+
+                <div className="flex justify-between mt-30">
+                    <div>
+                        <div>
+                            <HeroCard />
+                        </div>
+                        <div class="my-5 border-t mt-[280px] mb-[200px] border-white/20"></div>
+                        <div>
+                            <HeroCard />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="max-w-sm mx-auto  rounded-2xl  p-4">
+
+                            <div class="w-full rounded-xl overflow-hidden">
+                                <img
+                                    src= {rasm}
+                                    alt="game"
+                                    class="w-full h-48 sm:h-56 object-cover"
+                                />
+                            </div>
+
+                            <div class="flex items-center gap-3 mt-4">
+                                <span class="inline-block bg-blue-600 text-xs font-semibold px-3 py-1 rounded-lg text-white">
+                                    John Smash
+                                </span>
+                                <span class="text-sm text-white/70">5min</span>
+                            </div>
+
+                            <h3 class="mt-2 text-xl font-bold leading-tight text-white">
+                                Lorem Ipsum is simply dummy text dummy text ?
+                            </h3>
+
+                        </div>
+                        <div className="max-w-sm mx-auto  rounded-2xl  p-4">
+
+                            <div class="w-full rounded-xl overflow-hidden">
+                                <img
+                                    src=""
+                                    alt="game"
+                                    class="w-full h-48 sm:h-56 object-cover"
+                                />
+                            </div>
+
+                            <div class="flex items-center gap-3 mt-4">
+                                <span class="inline-block bg-blue-600 text-xs font-semibold px-3 py-1 rounded-lg text-white">
+                                    John Smash
+                                </span>
+                                <span class="text-sm text-white/70">5min</span>
+                            </div>
+
+                            <h3 class="mt-2 text-xl font-bold leading-tight text-white">
+                                Lorem Ipsum is simply dummy text dummy text ?
+                            </h3>
+
+                        </div>
+                        <div className="max-w-sm mx-auto  rounded-2xl  p-4">
+
+                            <div class="w-full rounded-xl overflow-hidden">
+                                <img
+                                    src="../assets/HomeIcons/card1.png"
+                                    alt="game"
+                                    class="w-full h-48 sm:h-56 object-cover"
+                                />
+                            </div>
+
+                            <div class="flex items-center gap-3 mt-4">
+                                <span class="inline-block bg-blue-600 text-xs font-semibold px-3 py-1 rounded-lg text-white">
+                                    John Smash
+                                </span>
+                                <span class="text-sm text-white/70">5min</span>
+                            </div>
+
+                            Anora Hasanova, [10.12.2025 15:46]
+                            <h3 class="mt-2 text-xl font-bold leading-tight text-white">
+                                Lorem Ipsum is simply dummy text dummy text ?
+                            </h3>
+
+                        </div>
+                        <div className="max-w-sm mx-auto  rounded-2xl  p-4">
+
+                            <div class="w-full rounded-xl overflow-hidden">
+                                <img
+                                    src="../assets/HomeIcons/card1.png"
+                                    alt="game"
+                                    class="w-full h-48 sm:h-56 object-cover"
+                                />
+                            </div>
+
+                            <div class="flex items-center gap-3 mt-4">
+                                <span class="inline-block bg-blue-600 text-xs font-semibold px-3 py-1 rounded-lg text-white">
+                                    John Smash
+                                </span>
+                                <span class="text-sm text-white/70">5min</span>
+                            </div>
+
+                            <h3 class="mt-2 text-xl font-bold leading-tight text-white">
+                                Lorem Ipsum is simply dummy text dummy text ?
+                            </h3>
+
+                        </div>
+                        <div className="max-w-sm mx-auto  rounded-2xl  p-4">
+
+                            <div class="w-full rounded-xl overflow-hidden">
+                                <img
+                                    src="../assets/HomeIcons/card1.png"
+                                    alt="game"
+                                    class="w-full h-48 sm:h-56 object-cover"
+                                />
+                            </div>
+
+                            <div class="flex items-center gap-3 mt-4">
+                                <span class="inline-block bg-blue-600 text-xs font-semibold px-3 py-1 rounded-lg text-white">
+                                    John Smash
+                                </span>
+                                <span class="text-sm text-white/70">5min</span>
+                            </div>
+
+                            <h3 class="mt-2 text-xl font-bold leading-tight text-white">
+                                Lorem Ipsum is simply dummy text dummy text ?
+                            </h3>
+
+                        </div>
+                        <div className="max-w-sm mx-auto  rounded-2xl  p-4">
+
+                            <div class="w-full rounded-xl overflow-hidden">
+                                <img
+                                    src="../assets/HomeIcons/card1.png"
+                                    alt="game"
+                                    class="w-full h-48 sm:h-56 object-cover"
+                                />
+                            </div>
+
+                            <div class="flex items-center gap-3 mt-4">
+                                <span class="inline-block bg-blue-600 text-xs font-semibold px-3 py-1 rounded-lg text-white">
+                                    John Smash
+                                </span>
+                                <span class="text-sm text-white/70">5min</span>
+                            </div>
+
+                            <h3 class="mt-2 text-xl font-bold leading-tight text-white">
+                                Lorem Ipsum is simply dummy text dummy text ?
+                            </h3>
+
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+            <div className="max-w-6xl mx-auto flex flex-col gap-10">
+
+                <div className="flex flex-col justify-center">
+                    <h1 className="font-poppins font-semibold text-[30px] md:text-[36px] leading-[1.8]">
+                        Lorem Ipsum
+                    </h1>
+                    <p className="font-poppins font-normal text-[16px] md:text-[18px] leading-[1.8] text-gray-300 mt-4">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    </p>
+                </div>
+
+                <div className="bg-white rounded-xl p-6 md:p-10 shadow-lg flex flex-col md:flex-row md:items-center md:justify-between gap-4 md:gap-6">
+                    <div className="flex-1">
+                        <h2 className="font-poppins font-medium text-[24px] md:text-[30px] leading-[1.2] text-black">
+                            Stay in the loop
+                        </h2>
+                        <p className="font-poppins font-normal text-[16px] md:text-[17px] leading-[1.5] text-black mt-2 max-w-md">
+                            Subscribe to receive the latest news and updates about TDA.
+                            We promise not to spam you!
+                        </p>
+                    </div>
+
+                    <form className="flex w-full md:w-auto flex-col md:flex-row gap-3 md:gap-0 mt-4 md:mt-0">
+                        <input
+                            type="email"
+                            placeholder="Enter email address"
+                            className="flex-1 h-16 md:h-14 bg-black text-white placeholder-gray-400 rounded-l-md px-4 outline-none font-poppins w-full text-lg md:text-base"
+                        />
+                        <button
+                            type="submit"
+                            className="h-8 md:h-14 px-4 bg-red-600 text-white font-poppins font-medium  rounded-r-md hover:bg-red-700 transition w-full md:w-auto text-base md:text-base"
+                        >
+                            Continue
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
 }
